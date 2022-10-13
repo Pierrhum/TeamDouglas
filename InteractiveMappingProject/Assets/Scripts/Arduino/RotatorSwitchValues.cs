2f80147da19ff02211a5b7a0632b002c99fa608d
Geegaz <geegazrd@gmail.com>
2022-10-13 10:56:37 +0200

    Added OnEncoderStep event

--- InteractiveMappingProject/Assets/Scripts/Arduino/RotatorSwitchValues.cs
+++ InteractiveMappingProject/Assets/Scripts/Arduino/RotatorSwitchValues.cs
@@ -13,23 +13,32 @@ public class RotatorSwitchValues : MonoBehaviour
     private int encoderID = 0;
     [SerializeField]
     private float turns = 4f;
-    
+
     [Header("Data")]
+    [SerializeField]
     [Range(0,1)]
-    public float value = 0f;
+    public float encoderValue = 0f;
+    private float lastEncoderValue = 0f;
 
     public UnityEvent<bool, float> OnEncoderRotate = new UnityEvent<bool, float>();
+    public UnityEvent<float> OnEncoderStep = new UnityEvent<float>();
 
+    private void Update() {
+        if (encoderValue != lastEncoderValue) {
+            if (OnEncoderRotate != null) OnEncoderRotate.Invoke(encoderValue > lastEncoderValue, encoderValue);
+            if (OnEncoderStep != null) OnEncoderStep.Invoke(encoderValue - lastEncoderValue);
+
+            lastEncoderValue = encoderValue;
+        }
+    }
+    
     void OnSerialValues(string[] values){
         int id = int.Parse(values[0]);
         if (id == encoderID) {
             int direction = int.Parse(values[1]);
             int position = int.Parse(values[2]);
 
-            value = Mathf.Repeat((float)position / (STEPS_BY_TURN * turns), 1f);
-            if (OnEncoderRotate != null) {
-                OnEncoderRotate.Invoke(direction > 0, value);
-            }
+            encoderValue = Mathf.Repeat((float)position / (STEPS_BY_TURN * turns), 1f);
 
             //Debug.Log(string.Format("{0}, {1}", direction > 0, value));
         }
